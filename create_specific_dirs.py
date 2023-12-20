7a84ee32450873c82be71286f411f741f725f7dc
kilian.lehn <kilian.lehn@annalect.com>
2023-12-20 20:42:00 +0100

    apply domain wide delegation

--- create_specific_dirs.py
+++ create_specific_dirs.py
@@ -1,64 +1,63 @@
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
-from googleapiclient.http import MediaFileUpload
 
 # Replace the following with your service account key file and the required scopes
-SERVICE_ACCOUNT_FILE = 'path/to/service_account.json'
+SERVICE_ACCOUNT_FILE = 'service_account.json'
 SCOPES = ['https://www.googleapis.com/auth/drive']
 
-def create_drive_service():
-    """Create a Google Drive service instance."""
+
+def create_drive_service(user_email):
+    """Create a Google Drive service instance with domain-wide delegation."""
     credentials = service_account.Credentials.from_service_account_file(
         SERVICE_ACCOUNT_FILE, scopes=SCOPES)
-    service = build('drive', 'v3', credentials=credentials)
+    delegated_credentials = credentials.with_subject(user_email)
+    service = build('drive', 'v3', credentials=delegated_credentials)
     return service
 
-def create_folder(name, parent_id=None):
+
+def create_folder(name, parent_id=None, user_email='admin@example.com'):
     """Create a folder in Google Drive."""
-    service = create_drive_service()
+    service = create_drive_service(user_email)
     file_metadata = {
         'name': name,
         'mimeType': 'application/vnd.google-apps.folder'
     }
     if parent_id:
         file_metadata['parents'] = [parent_id]
-    
+
     folder = service.files().create(body=file_metadata, fields='id').execute()
     return folder.get('id')
 
-def create_drive_file(name, mime_type, parent_id=None, content=None):
-    """Create a file in Google Drive."""
-    service = create_drive_service()
-    file_metadata = {
-        'name': name,
-        'mimeType': mime_type,
-        'parents': [parent_id] if parent_id else []
+
+def share_folder_with_user(folder_id, user_email, user_email_to_share):
+    """Share the specified folder with a user."""
+    service = create_drive_service(user_email)
+    user_permission = {
+        'type': 'user',
+        'role': 'writer',
+        'emailAddress': user_email_to_share
     }
+    service.permissions().create(
+        fileId=folder_id,
+        body=user_permission,
+        fields='id'
+    ).execute()
 
-    # For documents and spreadsheets with content
-    if content and mime_type in ['application/vnd.google-apps.document', 'application/vnd.google-apps.spreadsheet']:
-        file = service.files().create(body=file_metadata, fields='id').execute()
-        document_id = file.get('id')
-        for request in content:
-            service.documents().batchUpdate(documentId=document_id, body={'requests': [request]}).execute()
-        return document_id
-    else:
-        file = service.files().create(body=file_metadata).execute()
-        return file.get('id')
 
-# Structure to be created in Google Drive
-structure = {
-    # ... (paste your structure here)
-}
+def create_drive_structure(folder_names, admin_email, share_with_email):
+    """Create folders in Google Drive and share them."""
+    for folder_name in folder_names:
+        folder_id = create_folder(folder_name, user_email=admin_email)
+        share_folder_with_user(folder_id, admin_email, share_with_email)
+        print(f"Created and shared folder '{folder_name}'.")
 
-def create_drive_structure(structure):
-    """Create the specified folder and file structure in Google Drive."""
-    for folder_name, files in structure.items():
-        folder_id = create_folder(folder_name)
-        for file_name, mime_type, content in files:
-            create_drive_file(file_name, mime_type, folder_id, content)
-        print(f"Created folder '{folder_name}' with files.")
 
 # Example usage
+folder_names = ['Folder1', 'Folder2', 'Folder3']  # Replace with your list of folder names
+
+# Example usage
+admin_email = 'kilian@familylehn.com'  # Replace with your admin email
+share_with_email = 'kilian@familylehn.com'  # User to share folders with
+
 if __name__ == "__main__":
-    create_drive_structure(structure)
+    create_drive_structure(folder_names, admin_email, share_with_email)
